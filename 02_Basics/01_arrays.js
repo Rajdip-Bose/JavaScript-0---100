cfc0940661fa5ad40c8247f6412f2c5d19a64fe1
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-09-26 17:49:44 +0000

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,26 @@
+const a = [1,3,5,4]
+console.log(a[2]);
+
+// console.log(a[2]);
+
+// let n = new Array(1,12,3,2,3,3,4)
+// a.push(1)
+// a.push(7)
+// a.pop(7)
+
+
+// console.log(a);
+// console.log(a.includes(9));
+// console.log(a.indexOf(9));
+
+console.log("a = ",a);
+
+const myn1 = a.slice(0,2)
+console.log("Slice = ",myn1);
+console.log("a = ",a);
+
+
+const myn2 = a.splice(0,2)
+console.log("Splice = ",myn1);
+console.log("a = ",a);
+
