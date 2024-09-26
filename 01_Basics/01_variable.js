502a5399c90621b96919f4128dc368ce1e94332c
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-09-26 17:38:52 +0000

    variables

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,14 @@
+const namee = 2323;
+let email = "raj@google.com"
+var ps = "123"
+city = "Kolkata"
+// namee = 3
+email = "vjenijn"
+ps ="434r"
+city = "nagpur"
+
+console.log(namee);
+console.log(email);
+console.log(ps);
+console.log(city);
+console.table([namee,email,ps,city])
