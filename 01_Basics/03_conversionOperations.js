b2c20612477b5fa06102827433a8dc304bd2f66f
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-09-26 17:40:55 +0000

    conversion Operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+// let a = true
+
+// let b = Number(a)
+
+
+// console.log(b);
+// console.log(typeof b);
+
+// let x = 23110
+// let z = Boolean(x)
+// console.log(z);
+
+
+// let vvalue = 2
+// let nvalue  = -vvalue
+
+// console.log(nvalue);
+
+console.log("2"+2+2);
+console.log(2+2+"2");
+
