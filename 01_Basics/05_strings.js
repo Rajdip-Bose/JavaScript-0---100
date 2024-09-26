0eba6e38963b5571919db7dbb9ec76e471402030
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-09-26 17:43:10 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,16 @@
+// let name = "rahul"
+// let age = 34
+// console.log(`His name is ${name} and he was ${age} years old.`);
+
+
+const newn = new String("hello-uhf-ehbr0-ih")
+console.log(newn[0]);
+console.log(newn.toUpperCase());
+console.log(newn.charAt(4));
+console.log(newn.indexOf("e"));
+console.log(newn.slice(-7,4));
+console.log(newn.trim());
+console.log(newn.replace("hello","hihihihihi"));
+console.log(newn.includes("hello"));
+console.log(newn.split("-"));
+
