f8663d23a47ac55b66e398134025656b0bc03d3f
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-09-26 17:43:59 +0000

    Numbers

---
+++ 01_Basics/06_numbers.js
@@ -0,0 +1,16 @@
+const score = 600
+console.log(score);
+
+const balance = new Number(500.78)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed());
+console.log(balance.toPrecision(4));
+
+const any = 93882930942
+console.log(any.toLocaleString('en-In'));
+
+
+let max =20
+let min = 10
+console.log(Math.floor(Math.random()*(max-min + 1))+min);
