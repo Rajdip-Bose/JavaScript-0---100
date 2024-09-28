7aaea215db82385949e3d7521769dd3535820a54
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-09-28 17:37:25 +0000

    Obj Assigning

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -21,3 +21,11 @@ let user = {
 }
 
 console.log(user.fullName.UserFullname.firstName);
+
+
+let obj1 = {1 : "a" , 2 : "b"}
+let obj2 = {3 : "c" , 4 : "d"}
+
+// let obj3 = {obj1 , obj2}
+let obj3 = Object.assign({},obj1 , obj2)
+console.log(obj3);
