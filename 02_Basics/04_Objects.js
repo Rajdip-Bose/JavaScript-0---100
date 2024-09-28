dfb5dfd7b7cd46e279cb564ab889e122983a8399
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-09-28 17:26:07 +0000

    Nested obj

---
+++ 02_Basics/04_Objects.js
@@ -0,0 +1,23 @@
+
+// let tinder = new Object()  Singleton
+
+let tinder = {}
+
+tinder.name = "Hero"
+tinder.id = 930
+tinder.age = 23
+tinder.mailid = "hero@tata.com"
+
+// console.log(tinder);
+
+
+let user = {
+    fullName:{
+        UserFullname:{
+            firstName:"Rajdip",
+            lastName:"Bose"
+        }
+    }
+}
+
+console.log(user.fullName.UserFullname.firstName);
