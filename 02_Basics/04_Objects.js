60433f0f103f5354c4ae63efc7f947094c568be5
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-09-28 17:40:14 +0000

    object spread

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -27,5 +27,7 @@ let obj1 = {1 : "a" , 2 : "b"}
 let obj2 = {3 : "c" , 4 : "d"}
 
 // let obj3 = {obj1 , obj2}
+let obj4 = {...obj1 , ...obj2}
 let obj3 = Object.assign({},obj1 , obj2)
 console.log(obj3);
+console.log(obj4);
