ca7b432e0e76a457adad68b15e997e0c051831ae
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-09-28 17:00:52 +0000

    symbol

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -1,10 +1,17 @@
+let mysym = Symbol("Key1")
+
 let obj = {
     naam:"rajdip",
     age:21,
-    sirname:"bose"
+    sirname:"bose",
+    [mysym]:"lelele"
 }
 
 console.log(obj.naam);
 console.log(obj["naam"]);
 console.log(obj);
 
+// obj.naam="ksksks"
+// console.log(obj);
+// Object.freeze(obj)
+console.log(typeof obj[mysym]);
