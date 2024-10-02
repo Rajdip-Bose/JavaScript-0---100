66f06f809e3c205bc3e2521a65b3e344c21305d2
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-02 11:06:34 +0000

    F

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,30 +1,35 @@
-function f(){
-    console.log("This is how function works");
+// function f(){
+//     console.log("This is how function works");
     
-}
+// }
 
-f()
+// f()
 
-function add(a,b){
-    console.log(a+b);
+// function add(a,b){
+//     console.log(a+b);
     
-}
-function addd(a,b){
-    console.log(a+b);
-    return a+b
+// }
+// function addd(a,b){
+//     console.log(a+b);
+//     return a+b
     
-}
+// }
 
-add(12,32)
-let result = addd(23,43)
-console.log("Result is ",result);
+// add(12,32)
+// let result = addd(23,43)
+// console.log("Result is ",result);
 
 
-function userLoggedin(useername){
-    return `${useername} just logged in..`
-}
+// function userLoggedin(useername){
+//     return `${useername} just logged in..`
+// }
 
-console.log(userLoggedin("RAJA JI"));
+// console.log(userLoggedin("RAJA JI"));
 
 // okayyyy nice yeahh i understood
 // day 2 of cheating
+function addtoCart(...num){
+    return num
+}
+
+console.log(addtoCart(10,21,24,45,5,66,78,7,424,23,23,13,1,3,12,1,2));
