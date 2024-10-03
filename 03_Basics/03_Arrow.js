49dfa09b897dbc85392ebffab8949944958842dc
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-03 14:16:20 +0000

    This

--- 03_Basics/03_Arrow.js
+++ 03_Basics/03_Arrow.js
@@ -1,1 +1,12 @@
-// hi lol 
+let website = {
+    username:"Rajdip",
+    price : 999,
+
+
+    websiteUser : function greetings(){
+        return   `hello ${this.username} , Welcome to out website.`
+    }
+}
+
+console.log(website.websiteUser());
+ 
