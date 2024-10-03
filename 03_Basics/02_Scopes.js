d441d5b909aa998a83c5199bc6f2e6401427b78b
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-03 13:49:36 +0000

    scopes

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -4,12 +4,29 @@ const b = 32
 
 if (true){
     let a = 939
-    console.log("Inner a = ",a );
+    // console.log("Inner a = ",a );
     
     const b= 9020
     // var c= 83038
 }
 
-console.log("Outer a = ",a);
-console.log(b);
+// console.log("Outer a = ",a);
+// console.log(b);  
 // console.log(c);
+
+
+
+function one(){
+    let welcome = 23
+    
+    function two(){
+        let hi = "hellow"
+        console.log(welcome);
+        
+    }
+
+    two()
+
+}
+one()
+two()
