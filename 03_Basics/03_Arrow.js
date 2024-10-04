054ec6e6adf1c875c9475b651d7f2c7ec0d0eea4
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-04 17:51:23 +0000

    arrow

--- 03_Basics/03_Arrow.js
+++ 03_Basics/03_Arrow.js
@@ -45,3 +45,9 @@ let addthree = (num1,num2,num3) => num1+num2+num3
 addtwo(9,10)
 console.log(addthree(9,10,10))
 console.log(typeof addthree)
+
+
+// tyring out lol i mean cheating
+
+let mul = (num1,num2) => console.log(num1*num2)
+mul(9,10)
