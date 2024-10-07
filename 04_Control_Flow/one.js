4cf8d2af539e694c421d794b25b7a62b25682711
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-07 16:42:01 +0000

    if else

--- 04_Control_Flow/one.js
+++ 04_Control_Flow/one.js
@@ -1,12 +1,31 @@
-let age = 11
 
-if (age >=18){
-    console.log("You Can drive the Car!");
+
+
+
+
+
+if(2==="2"){
+    console.log("Done");
     
 }
 else{
-    console.log("Get LOST You idiot!!!!!!!!");   
+    console.log("Not valid");
+    
 }
+
+
+
+
+
+// let age = 11
+
+// if (age >=18){
+//     console.log("You Can drive the Car!");
+    
+// }
+// else{
+//     console.log("Get LOST You idiot!!!!!!!!");   
+// }
 
 // infinity looooop lol/
 // for (age ; age > 0; age++) {
