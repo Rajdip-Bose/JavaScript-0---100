b179784b63a4474175fa0e25b0e13a2de8fb4b7f
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-08 14:36:47 +0000

    same

--- 04_Control_Flow/Truthy.js
+++ 04_Control_Flow/Truthy.js
@@ -18,7 +18,7 @@ console.log(val1);
 console.log(val2);
 console.log(val3);
 
-let myKnowledge = 1110 
+let myKnowledge = 10 
 myKnowledge >=100 ? console.log("Upun ich Bhagwan Ha") : console.log("Bhai tum gadhe ho kia");
 
 
