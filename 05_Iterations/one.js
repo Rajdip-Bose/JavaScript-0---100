ea25aabb79efa4c87c1603808b528ebc3d6ed383
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-09 15:08:59 +0000

    for loop

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -6,11 +6,13 @@ for (let index = 1; index <= 10; index++) {
 }
 
 for (let i = 1; i <= 10; i++) {
+    console.log(`Multiplication Table for ${i}`);
     for(let j =1; j<=10;j++ ){
 
         // let j = 2
         console.log(`${i} X ${j} = ${i*j}`);
     }
+    
     
     
 }
