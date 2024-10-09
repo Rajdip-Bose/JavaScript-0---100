9d5ec91e1d8777a88e50ac8eaa01e18c5930e281
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-09 15:33:13 +0000

    break

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -6,6 +6,11 @@ for (let index = 1; index <= 10; index++) {
 }
 
 for (let i = 1; i <= 10; i++) {
+    if (i == 5) {
+        console.log("5 detected");
+        break;
+        
+    }
     console.log(`Multiplication Table for ${i}`);
     for(let j =1; j<=10;j++ ){
 
