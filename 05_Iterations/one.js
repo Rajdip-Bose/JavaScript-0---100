afc4e6cfc38796573e4194cc3500a097c976ef22
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-10 15:46:41 +0000

    while loop

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -1,33 +1,39 @@
-for (let index = 1; index <= 10; index++) {
-    const element = index;
-    console.log(element);
+// for (let index = 1; index <= 10; index++) {
+//     const element = index;
+//     console.log(element);
     
     
-}
+// }
 
-for (let i = 1; i <= 10; i++) {
-    if (i == 5) {
-        console.log("5 detected");
-        break;
+// for (let i = 1; i <= 10; i++) {
+//     if (i == 5) {
+//         console.log("5 detected");
+//         break;
         
-    }
-    console.log(`Multiplication Table for ${i}`);
-    for(let j =1; j<=10;j++ ){
+//     }
+//     console.log(`Multiplication Table for ${i}`);
+//     for(let j =1; j<=10;j++ ){
 
-        // let j = 2
-        console.log(`${i} X ${j} = ${i*j}`);
-    }
+//         // let j = 2
+//         console.log(`${i} X ${j} = ${i*j}`);
+//     }
 
  
     
     
     
-}
+// }
 
-let array =["Batman","Superman","Spiderman"]
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    console.log(element);
+// let array =["Batman","Superman","Spiderman"]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
     
+    
+// }
+let i = 1
+while (i <=10) {
+    console.log(i);
+    i++;
     
 }
