4598eaa850261de13733e99535afd615501cf9c5
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-10 16:05:03 +0000

    for of loop

---
+++ 05_Iterations/two.js
@@ -0,0 +1,13 @@
+let array = [1,2,3,4,5,6,7,8,9,10]
+
+for (const element of array) {
+    console.log(element);
+    
+}
+
+let string = "RAJDIP BOSE"
+
+for (const s of string) {
+    console.log(s);
+    
+}
