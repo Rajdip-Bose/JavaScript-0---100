bfe436aba3befbc3717ee01dc48c753a171110c0
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-12 13:33:59 +0000

    filter

--- 05_Iterations/six.js
+++ 05_Iterations/six.js
@@ -10,5 +10,12 @@
 
 
 let myNums = [1,2,3,4,5,6,7,8,9,10]
-let newN = myNums.filter( (nums) => nums > 4)
-console.log(newN);
+myNums.forEach( (num) => {
+    if (num > 4) {
+        console.log(num);
+        
+    }
+} )
+
+// let newN = myNums.filter( (nums) => nums > 4)
+// console.log(newN);
