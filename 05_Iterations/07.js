be0ba54d06c7767d1031c37d2f9201739ad1f28e
RAJDIP BOSE <rajdipbose2003@gmail.com>
2024-10-13 16:09:26 +0000

    REDUCE

--- 05_Iterations/07.js
+++ 05_Iterations/07.js
@@ -2,7 +2,9 @@ let array = [1,2,3,4,5,6,7,8,9,10]
 
 // let newN = array.map(num => num +10)
 // let newN = array.map(num => num +10).map(num => num*10)
-let newN = array.map(num => num +10)
-                .map(num => num*10)
-                .filter( num => num > 150)
-console.log(newN);
+// let newN = array.map(num => num +10)
+//                 .map(num => num*10)
+//                 .filter( num => num > 150)
+// console.log(newN);
+
+
